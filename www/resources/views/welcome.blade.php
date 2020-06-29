8e7a83e563becdb3d2594c6da81f0139c0506e8b
Govar Jabar <govarjabara@gmail.com>
2020-06-29 11:26:53 +0300

    disable width=device

--- www/resources/views/welcome.blade.php
+++ www/resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html dir="rtl">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
 
     <title>قاعدة بيانات الميزان الصرفي</title>
     <style>
